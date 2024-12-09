923bf5f7874b169d454bf123da23fe59f711056c
Balázs Kakuk <khoszrau@gmail.com>
2024-12-09 09:14:56 +0100

    gggg

--- pvclust.R
+++ pvclust.R
@@ -11,19 +11,19 @@
 ##
 fit.gene <- pvclust(t(countData),
                     method.hclust="complete", method.dist="uncentered", nboot=1000)
-plot(fit.gene)
+#plot(fit.gene)
 hc <- fit.gene$hclust
 #pvrect(fit.gene, alpha=0.85, pv="si")
-rect.hclust(hc, k = cluster_num, border = 2:6) # draws colored rectangles around the 5 clusters
+#rect.hclust(hc, k = cluster_num, border = 2:6) # draws colored rectangles around the 5 clusters
 
 # Cut the tree where it makes sense biologically
-clusters <- cutree(hc, k = cluster_num) # for example, 5 clusters
+#clusters <- cutree(hc, k = cluster_num) # for example, 5 clusters
 
 
-cluster_dt <- data.table(cluster=clusters, gene=rownames(countData))
+#cluster_dt <- data.table(cluster=clusters, gene=rownames(countData))
 
 ## store the distance matrix as well for the silhoutte
-dist_matrix <- pvclust:::dist.pvclust(t(countData), method=fit.gene$hclust$dist.method)
+#dist_matrix <- pvclust:::dist.pvclust(t(countData), method=fit.gene$hclust$dist.method)
 
 # Remove genes with zero variance
 ## correlation distance -->> not good
