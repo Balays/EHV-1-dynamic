b5da6e45af62b278c3b56cc32650e22c0578f506
Balázs Kakuk <khoszrau@gmail.com>
2024-12-17 17:32:45 +0100

    c

---
+++ NAGATA.R
@@ -0,0 +1,34 @@
+
+nagata.fwd <- data.table(as.data.frame(rtracklayer::import.gff3('./NAGATA/Final_cluster.NAGATA.fwd.gff3')))
+
+nagata.rev <- data.table(as.data.frame(rtracklayer::import.gff3('./NAGATA/Final_cluster.NAGATA.rev.gff3')))
+
+
+nagata.gff <- rbind(nagata.fwd, nagata.rev)
+
+
+nagata.gff <- nagata.gff[type %in% c('exon', 'mRNA'),]
+
+nagata.gff[,Parent := as.character(Parent)]
+
+nagata.gff[type == 'mRNA', Parent := ID]
+
+nagata.gff[,ID := Parent]
+
+nagata.gff[,Parent := NULL][,Name := NULL]
+
+nagata.mrna <- nagata.gff[type == 'mRNA',]
+
+stopifnot(
+  nrow(nagata.mrna) == length(unique(nagata.mrna[,ID]))
+)
+
+
+
+
+nagata.ex   <- nagata.gff[type == 'exon',]
+
+
+
+
+rtracklayer::export.gff3(as.data.frame(nagata.gff), 'NAGATA_dRNA.gff3')
